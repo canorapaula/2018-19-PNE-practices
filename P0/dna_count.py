644d860c126069aa87bdd012c2185d3cc95de5d1
pcanora <paulacanora@hotmail.es>
2019-01-30 17:25:11 +0100

    Count the number of bases in a DNA sequence.

---
+++ P0/dna_count.py
@@ -0,0 +1,24 @@
+DNA_input = input('Please introduce a DNA sequence:')
+DNA_input = upper.DNA_input
+
+print('Total length:', len(DNA_input))
+
+A_counter = 0
+G_counter = 0
+C_counter = 0
+T_counter = 0
+
+for x in DNA_input:
+    if x == 'A':
+        A_counter =+1
+    if x == 'G':
+        G_counter = +1
+    if x == 'C':
+        C_counter = +1
+    if x == 'T':
+        T_counter = +1
+
+print('A:', A_counter)
+print('G:', G_counter)
+print('C:', C_counter)
+print('T:', T_counter)
