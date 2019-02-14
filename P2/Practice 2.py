53470594d166308f18d92f4cf3988a5694fcd84f
pcanora <paulacanora@hotmail.es>
2019-02-14 14:07:43 +0100

    Practice 2. Send server a sequence, its reverse and its complement

--- P2/Practice 2.py
+++ P2/Practice 2.py
@@ -1,7 +1,10 @@
-# Practice 2
+# Practice 2. Send server a sequence, its reverse and its complement
+
 import socket
 
+
 class Seq:
+
     """A class for representing sequences."""
     def __init__(self, strbases):
         print('New sequence created.')
@@ -9,21 +12,42 @@ class Seq:
         self.strbases = strbases
 
 
-IP = "212.128.253.65"
+IP = "212.128.253.114"
 PORT = 8089
 while True:
     # Create the socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('Socket created')
-
+    print('\nSocket created')
     s.connect((IP, PORT))
 
+    # Asking the user for the sequence
     message = input('Please insert a sequence to send to the server:')
     s.send(str.encode(message))
 
+    # Calculating and sending the reversed sequence
+    reversed_seq = message[::-1]
+    s.send(str.encode("\nThe reversed sequence is:"))
+    s.send(str.encode(reversed_seq))
+
+    # Calculating and sending the complement sequence
+    complement = []
+    for x in message:
+        if x == 'A':
+            complement.append('T')
+        elif x == 'T':
+            complement.append('A')
+        elif x == 'C':
+            complement.append('G')
+        elif x == 'G':
+            complement.append('C')
+
+    complement = ''.join(complement)
+    s.send(str.encode('\nThe complement is:'))
+    s.send(str.encode(complement))
+
     # Data from the server
     msg = s.recv(2048).decode("utf-8")
-    print("MESSAGE FROM THE SERVER:\n")
+    print("\nMessage from the server:")
     print(msg)
 
     s.close()
