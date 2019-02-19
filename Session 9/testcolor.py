0e6a934b0d20f72fa36f33d695398474f45cfd94
pcanora <paulacanora@hotmail.es>
2019-02-19 16:07:27 +0100

    Writing texts in different colours

---
+++ Session 9/testcolor.py
@@ -0,0 +1,12 @@
+# Write texts in different colours
+
+import termcolor
+
+termcolor.cprint("Hey! this is printed in green!", 'green')
+termcolor.cprint("Hey! this is printed in yellow!", 'yellow')
+termcolor.cprint("Hey! this is printed in cyan!", 'cyan')
+termcolor.cprint("Hey! this is printed in magenta!", 'magenta')
+termcolor.cprint("Hey! this is printed in red!", 'red')
+termcolor.cprint("Hey! this is printed in white!", 'white')
+termcolor.cprint("Hey! this is printed in grey!", 'grey')
+termcolor.cprint("Hey! this is printed in blue!", 'blue')
