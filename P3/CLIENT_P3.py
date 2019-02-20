3960f719309b55385afd51974a8a0ed71e58be2b
pcanora <paulacanora@hotmail.es>
2019-02-20 18:43:45 +0100

    P3 SERVER

--- P3/CLIENT_P3.py
+++ P3/CLIENT_P3.py
@@ -1,19 +1,21 @@
 import socket
 
 # SERVER IP, PORT
-IP = "212.128.253.66"
+IP = "212.128.253.65"
 PORT = 8089
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IP, PORT))
+
 
 while True:
     # The client is blocking the server....  NOT A GOOD DESIGN!!!
-    msg = input("> ")
+    msg = """ATGC
+    len
+    countA
+    percA"""
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # establish the connection to the Server (IP, PORT)
-    s.connect((IP, PORT))
-
-    # Send the request message to the server
+# Send the request message to the server
     s.send(str.encode(msg))
 
     # Receive the servers respoinse
