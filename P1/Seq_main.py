3a382ed0a4d6d067e865e8739214999bc9f668f9
pcanora <paulacanora@hotmail.es>
2019-02-20 17:36:33 +0100

    Main program for Practice 1

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -1,3 +1,9 @@
 # Main program for Practice 1
 
 from Seq import Seq
+
+s1 = input('Please, enter Sequence 1:')
+str1 = s1.strbases
+print('Sequence 1: ', str1)
+l1 = s1.len()
+print('\tLength: ', l1)
