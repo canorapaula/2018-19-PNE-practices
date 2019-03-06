683deac84093701dda238682398df9ba23d3538b
pcanora <paulacanora@hotmail.es>
2019-03-06 17:37:24 +0100

    Session 13

--- Session-13/webserver-ex2.py
+++ Session-13/webserver-ex2.py
@@ -2,9 +2,33 @@ import http.server
 import socketserver
 
 PORT = 8000
-
+content = ''
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def process_client(cs):
+
+        msg = cs.recv(2048).decode("utf-8")
+
+        if msg.startswith("GET / "):
+            file = open('index2.html', 'r')
+            content = file.read()
+        else:
+            file = open('error.html', 'r')
+            content = file.read()
+        print()
+        print("Request message: ", msg)
+
+        status_line = "HTTP/1.1 200 ok\r\n"
+
+        header = "Content-Type: text/html\r\n"
+        header += "Content-Length: {}\r\n".format(len(str.encode(content)))
+
+        response_msg = status_line + header + "\r\n" + content
+
+        cs.send(str.encode(response_msg))
+        cs.close()
+
 
     def do_GET(self):
         print("GET received")
@@ -12,8 +36,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         print("Request line:" + self.requestline)
         print("   Cmd:   " + self.command)
         print("   Path:   " + self.path)
-
-        content = "I am the Happy Server! :-)"
 
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain')
