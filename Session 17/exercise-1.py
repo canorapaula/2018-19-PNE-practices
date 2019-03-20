54f21c0177f7d7beecb22bdc4acead30b5074752
pcanora <paulacanora@hotmail.es>
2019-03-20 18:24:45 +0100

    Exercise 1

--- Session 17/exercise-1.py
+++ Session 17/exercise-1.py
@@ -3,16 +3,15 @@ import termcolor
 
 f = open("person.json", 'r')
 
-# Object with all the info
 person = json.load(f)
 
-# The end='' is for it to be printed in the same line
-for data in person['Person_1']:
+print("Total people in the database:", len(person))
+
+for data in person:
     termcolor.cprint("Name: ", 'green', end='')
     print(data['Firstname'], data['Lastname'])
     termcolor.cprint("Age: ", 'green', end='')
     print(data['age'])
-
     termcolor.cprint("Phone Numbers: ", 'green', end='')
     print(len(person['phoneNumber']))
     for i, num in enumerate(person['phoneNumber']):
@@ -22,3 +21,4 @@ for data in person['Person_1']:
         print(num['type'])
         termcolor.cprint("\tNumber: ", 'red', end='')
         print(num['number'])
+    print()
