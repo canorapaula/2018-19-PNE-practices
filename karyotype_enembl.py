eb1592cf525a2d7103d6be49dc98d7a5aa2d09fe
Usuario <paulacanora@hotmail.es>
2019-04-19 17:24:26 +0200

    Karyotpe tryouts for Final Project

--- karyotype_enembl.py
+++ karyotype_enembl.py
@@ -11,18 +11,22 @@ SPECIE = ''
 ENDPOINT3 = "?content-type=application/json"
 METHOD = "GET"
 
-blep = listspecies1()
-for x in blep:
-    if x in LINK:
-        SPECIE = x
-        headers = {'User-Agent': 'http-client'}
-        conn = http.client.HTTPSConnection(HOSTNAME)
+listspecies1()
+blep = listspecies1().split(',')
+termcolor.cprint('BLEP {}'.format(blep), 'yellow')
 
-        conn.request(METHOD, ENDPOINT + ENDPOINT2, None, headers)
-        r1 = conn.getresponse()
-        text_json = r1.read().decode("utf-8")
-        conn.close()
-        user = json.loads(text_json)
+if blep in LINK:
+    SPECIE = blep
+    headers = {'User-Agent': 'http-client'}
+    conn = http.client.HTTPSConnection(HOSTNAME)
 
-        for x in user['karyotype']:
-            print('X', x)
+    termcolor.cprint('SPECIE {}'.format(SPECIE), 'green')
+
+    conn.request(METHOD, HOSTNAME + ENDPOINT + ENDPOINT2 + SPECIE + ENDPOINT3, None, headers)
+    r1 = conn.getresponse()
+    text_json = r1.read().decode("utf-8")
+    conn.close()
+    user = json.loads(text_json)
+    termcolor.cprint('LINK {}'.format(HOSTNAME + ENDPOINT + ENDPOINT2 + SPECIE + ENDPOINT3), 'green')
+    for k in user['karyotype']:
+        print('K', k)
